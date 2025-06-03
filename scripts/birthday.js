cd49f62c3b95ebf185195ad99bc73618d3af5b62
Ariel Saputra <109426054+arielxzy@users.noreply.github.com>
2025-06-03 09:52:52 +0800

    update scripts/birthday.js

--- scripts/birthday.js
+++ scripts/birthday.js
@@ -249,7 +249,7 @@ const animationTimeline = () => {
       {
         scale: 1,
         rotationY: 0,
-        color: "#ff69b4",
+        color: "#89CFF0",
         ease: Expo.easeOut,
       },
       0.1,
