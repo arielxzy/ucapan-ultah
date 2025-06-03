01bdbf42dfb0cf3e8403f2efbd2e1609abfa9e01
Ariel Saputra <109426054+arielxzy@users.noreply.github.com>
2025-06-03 11:56:30 +0800

    update scripts/birthday.js

--- scripts/birthday.js
+++ scripts/birthday.js
@@ -31,8 +31,8 @@ const animationTimeline = () => {
     .join("</span><span>")}</span>`;
 
   hbd.innerHTML = `<span>${hbd.innerHTML
-    .split("")
-    .join("</span><span>")}</span>`;
+  .split(/( )/)
+  .join("</span><span>")}</span>`;
 
   const ideaTextTrans = {
     opacity: 0,
